56dc9da814c875d5fac77e5fbafb20ea6fe11498
DarrenViljoen32 <31759385+DarrenViljoen32@users.noreply.github.com>
2024-03-01 17:31:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ config();
 const PORT = process.env.PORT
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:8080',
-    credentials:true
-}))
+app.use(cors())
 app.use(express.json())
 app.use(express.static('./views'))
 
